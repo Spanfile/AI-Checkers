17fd121e6707421bc1211d316059d2d2d96b7ac3
Spanfile <k.nemo@nettikioski.com>
2014-12-01 21:01:08 +0200

    Added map format for super pieces

--- AI Checkers/MoveInfo.cs
+++ AI Checkers/MoveInfo.cs
@@ -10,18 +10,22 @@ namespace AI_Checkers
     public class MoveInfo
     {
         bool useLineSeparator = true;
-        byte[] map;
+        bool usePieceSeparator = true;
+        Tuple<int, int>[] map;
 
         public MoveInfo()
         {
             useLineSeparator = Config.GetBool("useLineSeparator");
+            usePieceSeparator = Config.GetBool("usePieceSeparator");
         }
 
         public void SetMap(List<Tile> tiles, List<Piece> pieces)
         {
             map = (from tile in tiles
-                   let piece = pieces.Find((p) => p.boardPos.Equals(tile.boardPos))
-                   select piece == null ? (byte)0 : piece.color == PieceColor.Red ? (byte)1 : (byte)2).ToArray();
+                   let piece = pieces.Find(p => p.boardPos.Equals(tile.boardPos))
+                   let type = piece == null ? 0 : piece.color == PieceColor.Red ? 1 : 2
+                   let super = piece == null ? 0 : piece.super ? 1 : 0
+                   select Tuple.Create<int, int>(type, super)).ToArray();
         }
 
         public string Serialize()
@@ -34,7 +38,7 @@ namespace AI_Checkers
             int index = 0;
             while (index < map.Length / 8)
             {
-                yield return String.Join("", map.Skip(index * 8).Take(8));
+                yield return String.Join("", map.Skip(index * 8).Take(8).Select(t => String.Format("{0}{1}{2}", t.Item1, t.Item2, usePieceSeparator ? "/" : "")));
                 index += 1;
             }
         }
--- AI Checkers/Piece.cs
+++ AI Checkers/Piece.cs
@@ -8,78 +8,78 @@ using SFML.Window;
 
 namespace AI_Checkers
 {
-	public enum PieceColor
-	{
-		Red,
-		Black
-	}
+    public enum PieceColor
+    {
+        Red,
+        Black,
+    }
 
-	public class Piece : Transformable, Drawable
-	{
-		public Vector2f boardPos;
-		public PieceColor color;
-		public bool picked;
-		public bool eaten;
-		public bool super;
-		public Vector2f size;
+    public class Piece : Transformable, Drawable
+    {
+        public Vector2f boardPos;
+        public PieceColor color;
+        public bool picked;
+        public bool eaten;
+        public bool super;
+        public Vector2f size;
 
-		Sprite normalSprite;
-		Sprite superSprite;
-		Vector2f displayPos;
+        Sprite normalSprite;
+        Sprite superSprite;
+        Vector2f displayPos;
 
-		public Piece(Vector2f position, Vector2f size, Color color, PieceColor type)
-		{
-			this.size = size;
-			this.boardPos = position;
-			SetBoardPos(position);
+        public Piece(Vector2f position, Vector2f size, Color color, PieceColor type)
+        {
+            this.size = size;
+            this.boardPos = position;
+            SetBoardPos(position);
 
-			this.color = type;
-			picked = false;
+            this.color = type;
+            picked = false;
 
-			normalSprite = new Sprite(new Texture("checkerspiece.png"));
-			normalSprite.Origin = new Vector2f(size.X / 2, size.Y);
-			normalSprite.Color = color;
+            normalSprite = new Sprite(new Texture("checkerspiece.png"));
+            normalSprite.Origin = new Vector2f(size.X / 2, size.Y);
+            normalSprite.Color = color;
 
-			superSprite = new Sprite(new Texture("checkerspiece_double.png"));
-			superSprite.Origin = new Vector2f(size.X / 2, size.Y);
-			superSprite.Color = color;
-		}
+            superSprite = new Sprite(new Texture("checkerspiece_double.png"));
+            superSprite.Origin = new Vector2f(size.X / 2, size.Y);
+            superSprite.Color = color;
+        }
 
-		public void SetBoardPos(Vector2f pos)
-		{
-			boardPos = pos;
-			displayPos = new Vector2f(pos.X * size.X, pos.Y * size.Y) + size / 2;
-		}
+        public void SetBoardPos(Vector2f pos)
+        {
+            boardPos = pos;
+            displayPos = new Vector2f(pos.X * size.X, pos.Y * size.Y) + size / 2;
+        }
 
-		public void ApplyTransform(Transform trans)
-		{
-			normalSprite.Position = new Vector2f(0, 0);
-			superSprite.Position = new Vector2f(0, 0);
+        public void ApplyTransform(Transform trans)
+        {
+            normalSprite.Position = new Vector2f(0, 0);
+            superSprite.Position = new Vector2f(0, 0);
 
-			var point = trans.TransformPoint(displayPos);
-			point.Y += size.Y * 0.3f;
-			point.X += size.X * 0.1f;
-			Position = point;
-		}
+            var point = trans.TransformPoint(displayPos);
+            point.Y += size.Y * 0.3f;
+            point.X += size.X * 0.1f;
+            Position = point;
+        }
 
-		public void SetPosition(Vector2f pos)
-		{
-			normalSprite.Position = pos;
-			superSprite.Position = pos;
-		}
+        public void SetPosition(Vector2f pos)
+        {
+            normalSprite.Position = pos;
+            superSprite.Position = pos;
+        }
 
-		public void Draw(RenderTarget target, RenderStates states)
-		{
-			if (!eaten)
-			{
-				if (!picked)
-					states.Transform *= this.Transform;
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            if (!eaten)
+            {
+                if (!picked)
+                    states.Transform *= this.Transform;
 
-				if (!super)
-					normalSprite.Draw(target, states);
-				else
-					superSprite.Draw(target, states);
-			}
-		}
-	}
+                if (!super)
+                    normalSprite.Draw(target, states);
+                else
+                    superSprite.Draw(target, states);
+            }
+        }
+    }
 }
